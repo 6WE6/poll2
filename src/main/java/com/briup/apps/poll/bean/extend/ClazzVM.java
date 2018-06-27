54e2ffc050bb40e9d3eaa7b45ab854c145014851
DELL <DELL@DESKTOP-0G92KRP>
2018-06-27 17:49:55 +0800

    查询所有班级信息（班级中携带年级所属班级信息和班主任信息）

--- src/main/java/com/briup/apps/poll/bean/extend/ClazzVM.java
+++ src/main/java/com/briup/apps/poll/bean/extend/ClazzVM.java
@@ -1,6 +1,4 @@
 package com.briup.apps.poll.bean.extend;
-
-import com.briup.apps.poll.bean.Clazz;
 import com.briup.apps.poll.bean.Grade;
 import com.briup.apps.poll.bean.User;
 
@@ -13,18 +11,32 @@ public class ClazzVM {
 	/**
 	 * 非外键属性直接映射
 	 */
-	private Clazz clazz;
+	private Long id;
+	private String name;
+	private String description;
 	/**
 	 * 关联属性处理如下
 	 * 通过各自的id找到各自所有的信息
 	 */
 	private Grade grade;
 	private User charge;
-	public Clazz getClazz() {
-		return clazz;
+	public Long getId() {
+		return id;
 	}
-	public void setClazz(Clazz clazz) {
-		this.clazz = clazz;
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	public Grade getGrade() {
 		return grade;
@@ -37,7 +49,6 @@ public class ClazzVM {
 	}
 	public void setCharge(User charge) {
 		this.charge = charge;
-	}
-	
+	}	
 
 }
