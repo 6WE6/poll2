965c6d9a387d2e2cad184b375c7210fd80f7b2ac
guooyongyue <guoyongyue1996@163.com>
2018-07-04 17:28:13 +0800

    更新错误
    更新部分错误

--- src/main/java/com/briup/apps/poll/web/controller/SurveyController.java
+++ src/main/java/com/briup/apps/poll/web/controller/SurveyController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.briup.apps.poll.bean.Answers;
 import com.briup.apps.poll.bean.Survey;
 import com.briup.apps.poll.bean.extend.SurveyVM;
 import com.briup.apps.poll.service.ISurveyService;
@@ -31,7 +30,7 @@ public class SurveyController {
 
 	@Autowired
 	private ISurveyService surveyService;
-
+	// @author yun
 	@GetMapping("findAllSurvey")
 	@ApiOperation(value="查询所有课调信息",notes="保存课调信息时无需输入id")
 	public MsgResponse findAllSurvey(){
@@ -46,7 +45,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}		
 	}
-
+	// @author yun
 	@GetMapping("findSurveyById")
 	@ApiOperation(value="通过id查询课调信息")
 	public MsgResponse findSurveyById(@RequestParam long id){
@@ -59,7 +58,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-
+	// @author yun
 	@GetMapping("deleteSurveyById")
 	@ApiOperation(value="通过id删除课调信息")
 	public MsgResponse deleteSurveyById(@RequestParam long id){
@@ -70,10 +69,10 @@ public class SurveyController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return MsgResponse.error(e.getMessage());
-			
+
 		}
 	}
-
+	// @author yun
 	@GetMapping("findSurveyByKeyword")
 	@ApiOperation(value="通过关键字查询课调信息")
 	public MsgResponse findSurveyByKeyword(@RequestParam String keywords){
@@ -90,6 +89,7 @@ public class SurveyController {
 	 * 保存或修改课调信息
 	 * @param survey
 	 * @return
+	 * @author yun
 	 */
 	@PostMapping("saveOrUpdateSurvey")
 	@ApiOperation(value="保存或更新课调信息，如果是保存新课调信息，请不要填id！")
@@ -102,7 +102,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-
+	// @author yun
 	@GetMapping("batchDeleteSurvey")
 	@ApiOperation(value="批量删除课调信息")
 	public MsgResponse batchDeleteSurvey(Long[] ids){
@@ -114,7 +114,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-
+	// @author yun
 	@GetMapping("findAllSurveyVM")
 	@ApiOperation(value="获得所有的课调详细信息")
 	public MsgResponse findAllSurveyVM(){
@@ -126,7 +126,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+	// @author yun
 	@GetMapping("findSurveyVMByKeyword")
 	@ApiOperation(value="通过关键字获得所有的课调详细信息")
 	public MsgResponse findSurveyVMByKeyword(@RequestParam String keywords){
@@ -138,7 +138,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-
+	// @author yun
 	@GetMapping("findAllByIdAnswers")
 	@ApiOperation(value="通过id获得所有的课调的答题卡")
 	public MsgResponse findAllByIdAnswers(@RequestParam Long id){
@@ -150,7 +150,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+	// @author yun
 	@GetMapping("findByIdSurveyVM")
 	@ApiOperation(value="通过id获得课调")
 	public MsgResponse findByIdSurveyVM(@RequestParam Long id){
@@ -162,11 +162,11 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+
 	@GetMapping("SurveyExamine")
 	@ApiOperation(value="课调审核")
 	public MsgResponse SurveyExamine(@RequestParam Long id){
-		
+
 		try{
 			if(surveyService.checkSurvey(id)==null)
 			{
@@ -174,14 +174,14 @@ public class SurveyController {
 			}else{
 				return MsgResponse.success("success", surveyService.checkSurvey(id));
 			}
-			
+
 		}catch (Exception e) {
 			// 
 			return MsgResponse.error(e.getMessage());
 		}
-		
+
 	}
-	
+
 	@GetMapping("openSurvey")
 	@ApiOperation(value="通过课调id开启课调")
 	public MsgResponse openSurvey(@RequestParam Long id){
@@ -192,7 +192,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+
 	@GetMapping("closeSurvey")
 	@ApiOperation(value="通过课调id关闭课调")
 	public MsgResponse closeSurvey(@RequestParam Long id){
@@ -203,7 +203,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+
 	@GetMapping("checkSurvey")
 	@ApiOperation(value="通过课调id将课调改为审核通过状态或审核未通过状态，输入的results必须为1或者0")
 	public MsgResponse checkSurvey(@RequestParam Long id,int results){
@@ -214,7 +214,7 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
+
 	@ApiOperation(value="通过班级Id查询出该班级下所有已经审核的课调")
 	@GetMapping("findSurveyByClazzId")	
 	public MsgResponse findSurveyByClazzId(long id){
@@ -227,23 +227,4 @@ public class SurveyController {
 			return MsgResponse.error(e.getMessage());
 		}
 	}
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 }
